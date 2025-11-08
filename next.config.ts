35f2381627b6cdccab919f349595777724b28d2c
AsithaLKonara <asithalakmalkonara11992081@gmail.com>
2025-11-09 01:39:03 +0530

    chore: adjust config for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import path from "path";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(process.cwd(), ".."),
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
